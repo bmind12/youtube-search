56080430f57788f5dfdf128335b12b40cc982f9f
Sergey Ryabov <sergey.ryabov@actum.cz>
2018-02-28 23:29:56 +0100

    Implement videos display

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { searchVideos } from '../AC/videos'
 
 /* Components */
+import { CircularProgress } from 'material-ui/Progress';
 import Search from './Search'
+import VideoList from './VideoList'
 
-const App = props =>
+const App = ({ searchVideos, videos }) =>
     <div>
-        <Search onSubmit={props.searchVideos} />
+        <Search onSubmit={searchVideos} />
+        {videos.isFetching
+            ? <CircularProgress />
+            : <VideoList data={videos.data} />
+        }
     </div>
 
 export default connect(({ videos }) => ({
