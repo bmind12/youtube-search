a4cca6884b983e63c62c2289bdf3d7d689d055ef
Sergey Ryabov <sergey.ryabov@actum.cz>
2018-02-28 22:24:17 +0100

    Create api constants

---
+++ src/const/api.js
@@ -0,0 +1,8 @@
+export const ENDPOINTS = {
+    YOUTUBE_VIDEOS: 'https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.search.list'
+}
+
+export const PARAMETERS = {
+    PART: 'snippet',
+    TYPE_VIDEO: 'video'
+}
