67b310f026e20b295f766fa5cf1ac4378e403b3f
Sergey Ryabov <sergey.ryabov@actum.cz>
2018-02-28 22:26:55 +0100

    Connect middleware to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import reducers from '../reducers'
 
-const enhancer = applyMiddleware(thunk)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default createStore(
-    reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    enhancer
+const enhancer = composeEnhancers(
+    applyMiddleware(thunk)
 )
+
+export default createStore(reducers, {}, enhancer)
