52d9b7bd4e899c1c131bc40a2e6edea1a7c1fad4
Sergey Ryabov <sergey.ryabov@actum.cz>
2018-02-28 23:28:44 +0100

    Extend API constants

--- src/const/api.js
+++ src/const/api.js
@@ -1,8 +1,10 @@
 export const ENDPOINTS = {
-    YOUTUBE_VIDEOS: 'https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.search.list'
+    YOUTUBE_VIDEOS: 'https://www.googleapis.com/youtube/v3/search'
 }
 
 export const PARAMETERS = {
+    KEY: 'AIzaSyCPSIbBEPup4xLGbtjASVeIwMwrt62DHHg',
+    MAX_RESULTS: 10,
     PART: 'snippet',
     TYPE_VIDEO: 'video'
 }
