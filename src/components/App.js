8ba59d2576043de73054f9fbe334007c01e84c77
Sergey Ryabov <sergey.ryabov@actum.cz>
2018-03-01 00:58:58 +0100

    Add rendering functions to App component

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,50 @@ import { connect } from 'react-redux'
 import { searchVideos } from '../AC/videos'
 
 /* Components */
-import { CircularProgress } from 'material-ui/Progress';
+import { CircularProgress } from 'material-ui/Progress'
+import Grid from 'material-ui/Grid'
+import Paper from 'material-ui/Paper'
 import Search from './Search'
 import VideoList from './VideoList'
+import VideoPlayer from './VideoPlayer'
 
-const App = ({ searchVideos, videos }) =>
-    <div>
-        <Search onSubmit={searchVideos} />
-        {videos.isFetching
-            ? <CircularProgress />
-            : <VideoList data={videos.data} />
+const App = ({ classes, searchVideos, videos }) => {
+
+    const renderVideoList = () => {
+        if (videos.data.length > 0) {
+            return (
+                <Grid item xs={12} sm={4}>
+                    <Paper>
+                        <VideoList data={videos.data} />
+                    </Paper>
+                </Grid>
+            )
+        } else if (videos.isFetching) {
+            return (
+                <CircularProgress />
+            )
+        } else {
+            return null
         }
-    </div>
+    }
+
+    const renderVideoPlayer = () =>
+        <Grid item xs={12} sm={8}>
+            <Paper>
+                <VideoPlayer />
+            </Paper>
+        </Grid>
+
+    return (
+        <div>
+            <Search onSubmit={searchVideos} />
+            <Grid container spacing={24}>
+                {renderVideoPlayer()}
+                {renderVideoList()}
+            </Grid>
+        </div>
+    )
+}
 
 export default connect(({ videos }) => ({
     videos
